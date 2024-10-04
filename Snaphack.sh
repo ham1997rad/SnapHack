549f57f5940c64701db4a29ab3be04f4206d6e0c
ham1997rad <88598756+ham1997rad@users.noreply.github.com>
2024-10-04 16:31:42 +0300

    Update Snaphack.sh

--- Snaphack.sh
+++ Snaphack.sh
@@ -46,7 +46,7 @@ printf "\e[1;91m                   SKAR HACK (Telegram-skar44)  \e[0m\n"
 
 
 function start() {
-read -p $'\e[1;92mUsername account: \e[0m' user
+read -p $'\e[1;92mUsername account: \e[0m' h.shwaheen
 checkaccount=$(curl -L -s https://www.snapchat.com/$user/ | grep -c "the page may have been removed")
 if [[ "$checkaccount" == 1 ]]; then
 printf "\e[1;91mInvalid Username! Try again\e[0m\n"
